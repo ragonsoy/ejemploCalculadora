1c5393062a9599326a7fcb5f14880f6ca4d06512
alumnoFI <alumnoFI@facultades.ort.edu.uy>
2017-04-26 19:01:07 -0300

    prueba del git

---
+++ StringCalculator/StringCalculatorDominio/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorDominio
+{
+    public class Calculator
+    {
+        int DEFAULT_VALUE = 0;
+
+        public int Add(string numbers)
+        {           
+            if (HasNumbers(numbers))
+                return AddMultipleNumbers(Particionar(numbers));                
+            return DEFAULT_VALUE;
+        }
+
+        private int AddMultipleNumbers(string[] numbers)
+        {
+            int retorno = 0;
+            foreach (string number in numbers)            
+                retorno = retorno + ConvertStringToInt(number);
+            return retorno;
+        }
+
+        private string[] Particionar(string numbers)
+        {
+            return numbers.Split(',');
+        }
+
+        private bool HasNumbers(string numbers)
+        {
+            return numbers.Length > 0;
+        }
+
+        private int ConvertStringToInt(string numbers)
+        {
+            return Int32.Parse(numbers);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorTest/CalculatorTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorDominio;
+
+namespace StringCalculatorTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestAddZeroNumbers()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(0, calculator.Add(""));
+        }
+
+        [TestMethod]
+        public void TestAddOneNumber()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(4, calculator.Add("4"));
+        }
+
+        [TestMethod]
+        public void TestAddTwoNumber()
+        {
+            Calculator calculator = new Calculator();
+            Assert.AreEqual(5, calculator.Add("4,1"));
+        }
+    }
+}
